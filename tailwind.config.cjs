a0d70ed190930a7494ef40f1b3b17bd11678d222
Rob Wagner <rob@sombia.com>
2023-01-19 03:19:38 -0500

    Fix code block formatting and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,37 @@ module.exports = {
                 body: ["PT Sans"],
                 heading: ["Nunito"],
             },
+            typography: (theme) => ({
+                DEFAULT: {
+                    css: {
+                        em: {
+                            fontStyle: "italic",
+                        },
+                        pre: {
+                            width: "100%",
+                        },
+                        "pre code::before": {
+                            "padding-left": "unset",
+                        },
+                        "pre code::after": {
+                            "padding-right": "unset",
+                        },
+                        code: {
+                            color: theme("colors.gray.300"),
+                            fontWeight: "bold",
+                            "border-radius": "0.25rem",
+                        },
+                        "code::before": {
+                            content: "```",
+                            "padding-left": "0.25rem",
+                        },
+                        "code::after": {
+                            content: "```",
+                            "padding-right": "0.25rem",
+                        },
+                    },
+                },
+            }),
         },
     },
     plugins: [require("@tailwindcss/typography")],
